b262cfda1997a4011c4000839a778bce00755e5f
An <an.arletta.cattie@gmail.com>
2019-02-08 10:27:17 +0100

    no message

--- admin/view/conclusion.php
+++ admin/view/conclusion.php
@@ -1,4 +1,8 @@
-<div class="conclusion"> 
+<div class="conclusie"> 
     <h4>Mijn persoonlijke visie op deze job na dit interview:</h4>
-
+    <p>De IT-sector heeft veel opportuniteiten om in verschillende richtingen te gaan, <br>
+    Er zullen genoeg uitdagingen je pad kruisen. Er valt ook altijd iets nieuws te leren <br>
+    En het beste wat je kan doen is je bij iedere mogelijkheid bijbenen aan de hand van extra studies op zelfbasis en/of avondlessen. <br>
+    <br>
+    Allersinds een job met genoeg variatie en creativiteit.</p>
 </div>
